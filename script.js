7865c99e4db094032e6b62213f7709117004e349
EduardoK <eduardodirectortag@gmail.com>
2023-04-14 15:30:54 -0500

    Estilo TYPED en CSS presentacion

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ MenuBtn.addEventListener('click',function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+// EFECTO LETRA 
+
+let typed = new Typed(".auto-input",{
+    strings:['FullStack Developer!', 'Diseñador Web!'],
+    typeSpeed: 20,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
+
