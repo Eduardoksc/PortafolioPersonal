3cc1804082d50c910c13196b2dd085594b8c5bc4
EduardoK <eduardodirectortag@gmail.com>
2023-05-08 23:37:36 -0500

    Codigo JS para envio de informacion del formulario y posterior limpieza del mismo,ajuste de estilos y action en HTML

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// BOTÓN RESPOSIVE
+
 let MenuBtn = document.getElementById('MenuBtn')
 
 MenuBtn.addEventListener('click', function (e) {
@@ -8,12 +10,14 @@ MenuBtn.addEventListener('click', function (e) {
 // EFECTO LETRA 
 
 let typed = new Typed(".auto-input", {
-    strings: ['Front-End Developer!', 'Diseñador Web!', 'Freelancer!'],
+    strings: ['Desarrollador Web Full-Stack !', 'Diseñador Web', 'Analista de Datos!', 'Freelancer!'],
     typeSpeed: 100,
     backSpeed: 40,
     backDelay: 1000,
     loop: true,
 })
+
+// EFECTO DE NAVEGACIÓN ACTIVA 
 
 let navLinks = document.querySelectorAll('nav ul li a')
 console.log(navLinks)
@@ -36,6 +40,38 @@ window.addEventListener('scroll', function () {
 
 });
 
+
+// Limpiar el formulario despues de enviado el mesaje 
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    // Aquí iría el código para enviar los datos a Formspree
+
+    try {
+        // Aquí iría el código para enviar los datos a Formspree
+        const response = await fetch(event.target.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        if (response.ok) {
+            // Aquí se limpiarían los campos del formulario si el envío fue exitoso
+            form.reset();
+            alert('¡Gracias por tu mensaje!');
+        } else {
+            alert('Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.');
+        }
+    } catch (error) {
+        alert('Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.');
+    }
+});
+
+
 /*const navLinks2 = document.querySelectorAll('nav a');
 
 navLinks.forEach(link => {
@@ -55,27 +91,27 @@ link.addEventListener('click', e => {
 
 // Envio de email
 
-var btn = document.getElementById('btn');
-btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let subject = document.getElementById('subject').value;
-    let message = document.getElementById('message').value;
-    let body = 'name:' + name + '<br/> email:' + email + '<br/> subject:' +subject + '<br/> message' + message;
+// var btn = document.getElementById('btn');
+// btn.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     let name = document.getElementById('name').value;
+//     let email = document.getElementById('email').value;
+//     let subject = document.getElementById('subject').value;
+//     let message = document.getElementById('message').value;
+//     let body = 'name:' + name + '<br/> email:' + email + '<br/> subject:' +subject + '<br/> message' + message;
 
-    Email.send({
-        Host: "smtp.elasticemail.com",
-        Username: "eduardodirectortag@elasticemail.com",
-        Password: "4CCE64788C775647F8EC617DF0834E46F24D",
-        To: 'eduardodirectortag@gmail.com',
-        From: email,
-        Subject: subject,
-        Body: body,
-    }).then(
-        message => alert(message)
-    );
-});
+//     Email.send({
+//         Host: "smtp.elasticemail.com",
+//         Username: "eduardodirectortag@elasticemail.com",
+//         Password: "4CCE64788C775647F8EC617DF0834E46F24D",
+//         To: 'eduardodirectortag@gmail.com',
+//         From: email,
+//         Subject: subject,
+//         Body: body,
+//     }).then(
+//         message => alert(message)
+//     );
+// });
 
 
 
